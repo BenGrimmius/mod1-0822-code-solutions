fc797c905afcacf0af9c6f343aaf5ba412fae10b
Ben Grimmius <bengrimmius@gmail.com>
2022-09-27 01:12:29 +0000

    Complete javascript-loops-exercise

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -3,7 +3,7 @@
 function filterOutStrings(values) {
   var myArray = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] !== typeof 'string') {
+    if (typeof values[i] !== 'string') {
       myArray.push(values[i]);
     }
   }
